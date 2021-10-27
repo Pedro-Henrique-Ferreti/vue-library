524710c13ac2642e97ed6ddc2a748d271af9ed9a
Pedro Henrique <phfs28@gmail.com>
2021-10-27 17:40:48 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <!-- https://coolors.co/3d5a80-98c1d9-e0fbfc-ee6c4d-293241 -->
+  <!-- https://laravelexamples.com/ -->
 </template>
 
 <script>
