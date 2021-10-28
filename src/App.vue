7d1c3a4baf073fddab3657e10fee39ee06ec7805
Pedro Henrique <phfs28@gmail.com>
2021-10-28 09:48:58 -0300

    Installed node sass

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 
   <!-- https://coolors.co/3d5a80-98c1d9-e0fbfc-ee6c4d-293241 -->
   <!-- https://laravelexamples.com/ -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
