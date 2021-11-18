7e2c873972d702057a974f0f7329439a3b7b8142
Pedro Henrique <phfs28@gmail.com>
2021-11-18 14:43:33 -0300

    Added another before and after example

--- src/components/display/BeforeAfter.vue
+++ src/components/display/BeforeAfter.vue
@@ -1,12 +1,12 @@
 <template>
   <base-container>
-    <p class="example-1">Before and After</p>
+    <p class="example example-1">Before and After</p>
     
-    <p class="example-2">This is a nice quote from someone really smart</p>
+    <p class="example example-2">This is a nice quote from someone really smart</p>
 
-    <p class="example-3">This link has a <a href="#" data-tool-tip="Here is the tooltip content">tool tip</a>.</p>
+    <p class="example example-3">This link has a <a href="#" data-tool-tip="Here is the tooltip content">tool tip</a>.</p>
 
-    <div class="example-4">
+    <div class="example example-4">
       <h1>We can number sections!</h1>
       <h2 class="section">Section title here</h2>
       <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
@@ -31,6 +31,10 @@
         <li>Hello there</li>
       </ol>
     </div>
+
+    <div class="example example-5">
+      <img class="image" src="//unsplash.it/300/300" />
+    </div>
   </base-container>
 </template>
 
@@ -41,8 +45,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-p {
+.example:not(:last-child) {
   margin-bottom: 48px;
+}
+p {
   font-weight: $font-semibold;
 }
 .example-1 {
@@ -120,6 +126,49 @@ p {
   }
   p {
     margin-bottom: 1rem;
+  }
+}
+.example-5 {
+  width: 300px;
+  height: 300px;
+  background-color: #333;
+  position: relative;
+  &::before, &::after {
+    border: 0px solid $white;
+    position: absolute;
+    content: '';
+    top: 1.25em;
+    bottom: 1.25em;
+    left: 1.25em;
+    right: 1.25em;
+    transition: transform .3s ease-in-out;
+  }
+  &::before {
+    border-top-width: 1px;
+    border-bottom-width: 1px;
+    transform: scale(0, 1);
+  }
+  &::after {
+    border-left-width: 1px;
+    border-right-width: 1px;
+    transform: scale(1, 0);
+  }
+  .image {
+    width: inherit;
+    height: inherit;
+    opacity: 1;
+    transition: opacity .3s ease-in-out;
+  }
+  &:hover {
+    &::before {
+      transform: scale(1.05, 1);
+    }
+    &::after {
+      transform: scale(1, 1.05);
+    }
+    .image {
+      opacity: .4;
+    }
   }
 }
 </style>
